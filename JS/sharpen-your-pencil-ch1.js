ec04395ae8fae910b3dee6acaf1df9ecdb634d5e
patbou02 <poboutin@gmail.com>
2019-03-08 12:37:17 -0800

    add addSongs() function with code to update bullets with song names based on the li IDs.

--- JS/sharpen-your-pencil-ch1.js
+++ JS/sharpen-your-pencil-ch1.js
@@ -1,3 +1,15 @@
 "use strict";
 
 console.info("=== Chapter 1: Sharpen Your Pencils ===");
+
+function addSongs() {
+  let song1 = document.getElementById('song1');
+  let song2 = document.getElementById('song2');
+  let song3 = document.getElementById('song3');
+
+  song1.innerHTML = "Blue Suede Strings, by Elvis Presley";
+  song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+  song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+}
+
+window.onload = addSongs();
