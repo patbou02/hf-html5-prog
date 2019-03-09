91997e6982384570ad6cd4001d5a9c2fcacf1cca
patbou02 <poboutin@gmail.com>
2019-03-09 00:24:43 -0800

    create random words.

--- JS/phrase-o-matic.js
+++ JS/phrase-o-matic.js
@@ -7,6 +7,11 @@ function makePhrases() {
   let words1 = ["24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win"];
   let words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
   let words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+
+  // Create Random Words
+  let rand1 = Math.floor(Math.random() * words1.length);
+  let rand2 = Math.floor(Math.random() * words2.length);
+  let rand3 = Math.floor(Math.random() * words3.length);
 }
 
 window.onload = makePhrases;
