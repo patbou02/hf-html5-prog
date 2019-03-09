46677b5d8a1117f4014296b2202b31d6f7c59ab2
patbou02 <poboutin@gmail.com>
2019-03-09 09:58:24 -0800

    rename JS file and update HTML that points to it. Create new JS file for sharpent your pencil exercise on chapter 2.

---
+++ JS/2-sharpen-your-pencil.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.info("=== Chap2ter : Sharpen Your Pencils ===");
+
+function handleButtonClick() {
+  let textInput = document.getElementById("songTextInput");
+  let songName = textInput.value;
+  console.log(`Adding ${songName}.`);
+}
