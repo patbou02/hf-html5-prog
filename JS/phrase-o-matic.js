41c36fd3a24db46176f27035b26d47ddbc912cf4
patbou02 <poboutin@gmail.com>
2019-03-09 00:19:08 -0800

    set variables holding arrays which each will hold words that will be used to create phrases.

--- JS/phrase-o-matic.js
+++ JS/phrase-o-matic.js
@@ -3,7 +3,10 @@
 console.info("=== Chapter 1: Phrase-O-Matic ===");
 
 function makePhrases() {
-  
+  // Set Arrays with words in side of them
+  let words1 = ["24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win"];
+  let words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+  let words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
 }
 
 window.onload = makePhrases;
