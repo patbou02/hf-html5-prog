356a10f27548603b50beec048b748374a07ed218
patbou02 <poboutin@gmail.com>
2019-03-09 09:45:18 -0800

    move code inside init function and assign said function to the onload property of the window object.

--- JS/2-webville-tunes.js
+++ JS/2-webville-tunes.js
@@ -2,9 +2,12 @@
 
 console.info("=== Chapter 2: Webville Tunes ===");
 
-let button = document.getElementById('addButton');
+window.onload = init;
 
-button.onclick = handleButtonClick;
+function init() {
+  let button = document.getElementById('addButton');
+  button.onclick = handleButtonClick;
+}
 
 function handleButtonClick() {
   console.log('button was clicked');
