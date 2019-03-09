2ec6344c2e79c45155b0cdcb62cca3cd3ca37687
patbou02 <poboutin@gmail.com>
2019-03-09 09:39:25 -0800

    create handleButtonClick function and assign it to the onclick event of the button.

--- JS/2-webville-tunes.js
+++ JS/2-webville-tunes.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.info("=== Chapter 2: Webville Tunes ===");
+
+let button = document.getElementById('addButton');
+
+button.onclick = handleButtonClick;
+
+function handleButtonClick() {
+  console.log('button was clicked');
+}
