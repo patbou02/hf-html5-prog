0c583cf52936e97b9c468b54b1907e1fa0bce688
patbou02 <poboutin@gmail.com>
2019-03-09 22:41:17 -0800

    add webville cinema JS starting file.

---
+++ 4-Serious-JavaScript/webville-cinema.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.info("Chapter 4: Webville Cinema");
+
+let lordOfTheRings = {
+  title: "Lord of the Rings",
+  genre: "Fantasy",
+  rating: 4,
+  showTime: ["3:00pm", "5:00pm", "7:00pm", "11:00pm"],
+};
