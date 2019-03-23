681657069b9149008a369d606cb28f9f8d5ddc13
patbou02 <poboutin@gmail.com>
2019-03-23 01:21:58 -0700

    add reference to external JS file and create function set to be invoked on page load.

--- 5-Geolocation/my-location.js
+++ 5-Geolocation/my-location.js
@@ -1,3 +1,13 @@
 "use strict";
 
 console.info("Chapter 5: My Location");
+
+window.onload = getMyLocation;
+
+function getMyLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayLocation);
+  } else {
+    console.log('No geolocation support.');
+  }
+}
