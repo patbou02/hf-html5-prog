7be43c2af9c7033a038bcb7a808e3847bf0c5d92
patbou02 <poboutin@gmail.com>
2019-03-31 12:02:40 -0700

    change container within HTML file to DIV since it is more appropriate than P tag now. update updateSales function to convert JSON string into JSON object and parse it in order to extract individual elements.

--- 6-communication-apis/gumball.js
+++ 6-communication-apis/gumball.js
@@ -17,5 +17,12 @@ window.onload = function() {
 
 function updateSales(responseText) {
   let salesContainer = document.getElementById('sales');
-  salesContainer.innerText = responseText;
+  let sales = JSON.parse(responseText);
+  for (let i = 0; i < sales.length; i++) {
+    let sale = sales[i];
+    let div = document.createElement("div");
+    div.setAttribute("class", "saleItem");
+    div.innerHTML = `${sale.name} sold ${sale.sales} gumballs`;
+    salesContainer.appendChild(div);
+  }
 }
