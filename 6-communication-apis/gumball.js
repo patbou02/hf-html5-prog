c2bb92df0b3bb851138001e63772705aff60586e
patbou02 <poboutin@gmail.com>
2019-03-31 10:13:56 -0700

    add destination element within HTML for sales reports. create new onload handler function responsible for creating new HTTP request.

--- 6-communication-apis/gumball.js
+++ 6-communication-apis/gumball.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.info("Project Name");
+console.info("Chapter 6: External APIs");
+
+window.onload = function() {
+  let url = 'http://localhost/sales.json';
+  let request = new XMLHttpRequest();
+
+  request.open("GET", url);
+  request.onload = function() {
+    if (request.status === 200) {
+      updateSales(request.responseText);
+    }
+  };
+  request.send(null);
+};
