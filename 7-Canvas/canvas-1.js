947dd1c39a588fcad5e5090b6d08b687b1633e8b
patbou02 <poboutin@gmail.com>
2019-04-06 00:39:03 -0700

    move JS code from within HTML file and into JS file.

--- 7-Canvas/canvas-1.js
+++ 7-Canvas/canvas-1.js
@@ -1,3 +1,22 @@
 "use strict";
 
 console.info("Chapter 7: Canvas API - Gumball");
+
+window.onload = function() {
+  let canvas = document.getElementById("lookwhatIdrew");
+  let context = canvas.getContext("2d");
+
+  context.fillRect(10, 10, 100, 100);
+};
+
+let selectObj = document.getElementById('backgroundColor');
+let index = selectObj.selectedIndex;
+let bgColor = selectObj[index].value;
+
+let selectObj = document.getElementById("shape");
+let index = selectObj.selectedIndex;
+let shape = selectObj[index].value;
+
+let selectObj = document.getElementById("foregroundcolor");
+let index = selectObj.selectedIndex;
+let fgColor = selectObj[index].value;
