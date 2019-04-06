a717ab87e07ed88d142449c48da2ea9df8ce7a99
patbou02 <poboutin@gmail.com>
2019-04-06 01:06:38 -0700

    get canvas element and ask for its 2d drawing context. get shape dropdown element and other variables.

--- 7-Canvas/canvas-2-tweetshirt.js
+++ 7-Canvas/canvas-2-tweetshirt.js
@@ -8,5 +8,10 @@ window.onload = function() {
 };
 
 function previewHandler() {
+  let canvas = document.getElementById('tshirtCanvas');
+  let context = canvas.getContext('2d');
 
+  let selectObj = document.getElementById('shape');
+  let index = selectObj.selectedIndex;
+  let shape = selectObj[index].value;
 }
