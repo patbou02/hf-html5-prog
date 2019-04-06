b0c2f83e829350811a402448a9945f6de600a0be
patbou02 <poboutin@gmail.com>
2019-04-06 13:38:46 -0700

    add JS code for each section of pseudo code and define degreesToRadians().

--- 7-Canvas/canvas-3-smiley.js
+++ 7-Canvas/canvas-3-smiley.js
@@ -13,13 +13,37 @@ function previewHandler() {
 
   context.beginPath();
 
+
+  context.beginPath();
+
   // draw head
+  context.arc(300, 300, 200, 0, degreesToRadians(360), true);
+  context.fillStyle = "beige";
+  context.fill();
+  context.stroke();
 
   // draw left eye
+  context.beginPath();
+  context.arc(200, 250, 25, 0, degreesToRadians(360), true);
+  context.stroke();
 
   // draw right eye
+  context.beginPath();
+  context.arc(400, 250, 25, 0, degreesToRadians(360), true);
+  context.stroke();
 
   // draw nose
+  context.beginPath();
+  context.moveTo(300, 300);
+  context.lineTo(300, 350);
+  context.stroke();
 
   // draw mouth
+  context.beginPath();
+  context.arc(300, 350, 75, degreesToRadians(20), degreesToRadians(160), false);
+  context.stroke();
+}
+
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
 }
