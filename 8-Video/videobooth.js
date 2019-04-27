034def4f9c45d4a2ef96570909a6f61a396a6029
patbou02 <poboutin@gmail.com>
2019-04-27 15:08:48 -0700

    fix casing typo with onclick event listener that calls setEffect, handleControl and setVideo.

--- 8-Video/videobooth.js
+++ 8-Video/videobooth.js
@@ -6,19 +6,19 @@ window.onload = function() {
   // Set onClick handler for effect links.
   let effectLinks = document.querySelectorAll("a.effect");
   for (let i = 0; i < effectLinks.length; i++) {
-    effectLinks[i].onClick = setEffect;
+    effectLinks[i].onclick = setEffect;
   }
 
   // Set onClick handler for control links.
   let controlLinks = document.querySelectorAll("a.control");
   for (let j = 0; j < controlLinks.length; j++) {
-    controlLinks[j].onClick = handleControl;
+    controlLinks[j].onclick = handleControl;
   }
 
   // Set onClick handler for video selection links.
   let videoLinks = document.querySelectorAll("a.videoSelection");
   for (let k = 0; k < videoLinks.length; k++) {
-    videoLinks[k].onClick = setVideo;
+    videoLinks[k].onclick = setVideo;
   }
 
   // Helper function to visually depress 'video1' and 'normal' buttons.
@@ -41,8 +41,7 @@ function setEffect(e) {
 }
 
 function handleControl(e) {
-  let id = e.target.getAttribute('id');
-
+  let id = e.target.getAttribute("id");
   if (id === "play") {
     pushUnpushButtons("play", ["pause"]);
   } else if (id === "pause") {
@@ -64,4 +63,36 @@ function setVideo(e) {
   } else if (id === "video2") {
     pushUnpushButtons("video2", ["video1"]);
   }
+}
+
+// Helper Functions
+function pushUnpushButtons(idToPush, idArrayToUnpush) {
+  if (idToPush != "") {
+    var anchor = document.getElementById(idToPush);
+    var theClass = anchor.getAttribute("class");
+
+    if (!theClass.indexOf("selected") >= 0) {
+      theClass = theClass + " selected";
+      anchor.setAttribute("class", theClass);
+      var newImage = "url(images/" + idToPush + "pressed.png";
+      anchor.style.backgroundImage = newImage;
+    }
+  }
+
+  for (let i = 0; i < idArrayToUnpush.length; i++) {
+    anchor = document.getElementById(idArrayToUnpush[i]);
+    theClass = anchor.getAttribute("class");
+    if (theClass.indexOf("selected") >= 0) {
+      theClass = theClass.replace("selected", "");
+      anchor.setAttribute("class", theClass);
+      anchor.style.backgroundImage = "";
+    }
+  }
+}
+
+function isButtonPushed(id) {
+  let anchor = document.getElementById(id);
+  let theClass = anchor.getAttribute("class");
+
+  return (theClass.indexOf("selected") >= 0);
 }
