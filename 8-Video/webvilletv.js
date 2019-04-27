5f6fd6b28fdd8a66069909e3c8769a9730718a77
patbou02 <poboutin@gmail.com>
2019-04-27 10:53:04 -0700

    set variables and onload event listener of the window so playlist holds list of videos to play, video element is accessed from DOM and src is set before loading and playing via respective methods.

--- 8-Video/webvilletv.js
+++ 8-Video/webvilletv.js
@@ -1,3 +1,30 @@
 "use strict";
 
 console.info("Chapter 8: Video API - Webville TV");
+
+/* Set Global Variables */
+let position = 0; // keep track of which video is playing
+let playlist;     // video playlist array will be stored here
+let video;        // reference to video element
+
+window.onload = function() {
+  console.log("page has loaded");
+
+  // Setup Playlist
+  playlist = [
+    "video/preroll.mp4",
+    "video/areyoupopular.mp4",
+    "video/destinationearth.mp4",
+  ];
+
+  // Grab video element.
+  video = document.getElementById("video");
+
+  // Set video event listener.
+  video.addEventListener("ended", nextVideo, false);
+
+  // set video source, load video and play it.
+  video.src = playlist[position];
+  video.load();
+  video.play();
+};
