f864bb090ed68ec6588d941691d9dea2ac23eabc
patbou02 <poboutin@gmail.com>
2019-04-27 14:15:32 -0700

    define setEffect, handleControl and setVideo functions which will be called on click events for the appropriate buttons.

--- 8-Video/videobooth.js
+++ 8-Video/videobooth.js
@@ -25,3 +25,43 @@ window.onload = function() {
   pushUnpushButtons("video1", []);
   pushUnpushButtons("normal", [])
 };
+
+function setEffect(e) {
+  let id = e.target.getAttribute("id");
+
+  if (id === "normal") {
+    pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+  } else if (id === "western") {
+    pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+  } else if (id === "noir") {
+    pushUnpushButtons("noir", ["normal", "western", "scifi"]);
+  } else if (id === "scifi") {
+    pushUnpushButtons("scifi", ["normal", "western", "noir"]);
+  }
+}
+
+function handleControl(e) {
+  let id = e.target.getAttribute('id');
+
+  if (id === "play") {
+    pushUnpushButtons("play", ["pause"]);
+  } else if (id === "pause") {
+    pushUnpushButtons("pause", ["play"]);
+  } else if (id === "loop") {
+    (isButtonPushed("loop")) ?
+      pushUnpushButtons("", ["loop"]) : pushUnpushButtons("loop", []);
+  } else if (id === "mute") {
+    (isButtonPushed("mute")) ?
+      pushUnpushButtons("", ["mute"]) : pushUnpushButtons("mute", []);
+  }
+}
+
+function setVideo(e) {
+  let id = e.target.getAttribute("id");
+
+  if (id === "video1") {
+    pushUnpushButtons("video1", ["video2"]);
+  } else if (id === "video2") {
+    pushUnpushButtons("video2", ["video1"]);
+  }
+}
