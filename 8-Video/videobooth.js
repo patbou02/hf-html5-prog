8aebd047a1854c769b9835c37579ae527dfdb6ce
patbou02 <poboutin@gmail.com>
2019-04-29 23:29:58 -0700

    define functions for noir, scifi and b&w cartoon special effects.

--- 8-Video/videobooth.js
+++ 8-Video/videobooth.js
@@ -190,3 +190,35 @@ function noir(pos, r, g, b, data) {
   data[(pos * 4) + 1] = brightness;
   data[(pos * 4) + 2] = brightness;
 }
+
+/* Effect - Western */
+function western(pos, r, g, b, data) {
+  let brightness = ((3*r) + (4*g) + b) >>> 3;
+  data[(pos * 4) + 0] = brightness + 40;
+  data[(pos * 4) + 1] = brightness + 20;
+  data[(pos * 4) + 2] = brightness - 20;
+}
+
+/* Effect - SciFi */
+function scifi(pos, r, g, b, data) {
+  let offset = pos * 4;
+  data[offset] = Math.round(255 - r);
+  data[offset + 0] = Math.round(255 - g);
+  data[offset + 2] = Math.round(255 - b);
+}
+
+/* Effect - B&W Cartoon */
+function bwcartoon(pos, r, g, b, outputData) {
+  let offset = pos * 4;
+  if (outputData[offset] < 120) {
+    outputData[offset] = 80;
+    outputData[++offset] = 80;
+    outputData[++offset] = 80;
+  } else {
+    outputData[offset] = 255;
+    outputData[++offset] = 255;
+    outputData[++offset] = 255;
+  }
+  outputData[++offset] = 255;
+  ++offset;
+}
