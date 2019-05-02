df1f67ae6c4220a2a3bdd8fd36f913a78f14e249
patbou02 <poboutin@gmail.com>
2019-05-02 02:05:52 -0700

    set items in local storage and retrieve one item and display it via alert().

--- 9-Web-Storage/notetoself.js
+++ 9-Web-Storage/notetoself.js
@@ -1,3 +1,10 @@
 "use strict";
 
-console.info("Project Name");
+console.info("Chapter 9: Web Storage - Note to Self");
+
+// store Items
+localStorage.setItem("sticky_0", "Pick up dry cleaning");
+localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
+
+let sticky = localStorage.getItem("sticky_0");
+alert(sticky);
