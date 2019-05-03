802e1f4a21100b696e557d571c97a78c201fddff
patbou02 <poboutin@gmail.com>
2019-05-03 01:25:52 -0700

    add different syntax example for local storage item setting and HTML and JS files for the shell game.

--- 9-Web-Storage/notetoself.js
+++ 9-Web-Storage/notetoself.js
@@ -8,3 +8,17 @@ localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
 
 let sticky = localStorage.getItem("sticky_0");
 alert(sticky);
+
+// second way of setting and getting local storage items.
+localStorage['sticky_2'] = "Time to by some groceries";
+let sticky2 = localStorage['sticky_2'];
+
+alert(sticky2);
+
+// use length property and key method to access all localStorage items.
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i);
+  let value = localStorage[key];
+
+  console.info(`Local Storage item: ${key} with a value of: ${value}.`);
+}
