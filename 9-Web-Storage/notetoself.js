918bb7742c96fba432407c27392e49029e1d152d
patbou02 <poboutin@gmail.com>
2019-05-12 01:17:16 -0700

    add createSticky function and invoke it on click of button.

--- 9-Web-Storage/notetoself.js
+++ 9-Web-Storage/notetoself.js
@@ -9,6 +9,9 @@ localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
 window.onload = init;
 
 function init() {
+  let button = document.getElementById('add_button');
+  button.onclick = createSticky;
+
   for (let i = 0; i < localStorage.length; i++) {
     let key = localStorage.key(i);
     if (key.substring(0, 6) === 'sticky') {
@@ -26,4 +29,12 @@ function addStickyToDOM(value) {
   span.innerHTML = value;
   sticky.appendChild(span);
   stickies.appendChild(sticky);
+}
+
+function createSticky() {
+  let value = document.getElementById('note_text').value;
+  let key = "sticky_" + localStorage.length;
+  localStorage.setItem(key, value);
+
+  addStickyToDOM(value);
 }
