579da930592481fb7a040e3dc6ab0273e3a921b0
patbou02 <poboutin@gmail.com>
2019-05-12 00:22:52 -0700

    add notetoself CSS file and link from HTML file, remove previous code in JS file in preparation for Stickies app.

--- 9-Web-Storage/notetoself.js
+++ 9-Web-Storage/notetoself.js
@@ -1,24 +1,3 @@
 "use strict";
 
 console.info("Chapter 9: Web Storage - Note to Self");
-
-// store Items
-localStorage.setItem("sticky_0", "Pick up dry cleaning");
-localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
-
-let sticky = localStorage.getItem("sticky_0");
-alert(sticky);
-
-// second way of setting and getting local storage items.
-localStorage['sticky_2'] = "Time to by some groceries";
-let sticky2 = localStorage['sticky_2'];
-
-alert(sticky2);
-
-// use length property and key method to access all localStorage items.
-for (let i = 0; i < localStorage.length; i++) {
-  let key = localStorage.key(i);
-  let value = localStorage[key];
-
-  console.info(`Local Storage item: ${key} with a value of: ${value}.`);
-}
