887aca6a7b0ed93193753ca0fe1c5e4e11741266
patbou02 <poboutin@gmail.com>
2019-05-12 00:40:43 -0700

    define init and addStickyToDOM functions. Add form and ul container to receive sticky notes.

--- 9-Web-Storage/notetoself.js
+++ 9-Web-Storage/notetoself.js
@@ -1,3 +1,29 @@
 "use strict";
 
 console.info("Chapter 9: Web Storage - Note to Self");
+
+// store Items
+localStorage.setItem("sticky_0", "Pick up dry cleaning");
+localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
+
+window.onload = init;
+
+function init() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    if (key.substring(0, 6) === 'sticky') {
+      let value = localStorage.getItem(key);
+      addStickyToDOM(value);
+    }
+  }
+}
+
+function addStickyToDOM(value) {
+  let stickies = document.getElementById('stickies');
+  let sticky = document.createElement('li');
+  let span = document.createElement('span');
+  span.setAttribute('class', 'sticky');
+  span.innerHTML = value;
+  sticky.appendChild(span);
+  stickies.appendChild(sticky);
+}
