3928696286d5da2ac68feac13b256965073cae71
patbou02 <poboutin@gmail.com>
2019-06-12 00:13:06 -0700

    Remove self-generating localStorage items. Create new getStickiesArray function and moved logic so that we now use an array with array elements having their keys include set prefix plus time in miliseconds.

--- 9-Web-Storage/notetoself-witharray.js
+++ 9-Web-Storage/notetoself-witharray.js
@@ -2,22 +2,18 @@
 
 console.info("Chapter 9: Web Storage - Note to Self with Array");
 
-// store Items
-localStorage.setItem("sticky_0", "Pick up dry cleaning");
-localStorage.setItem("sticky_1", "Cancel cable TV, who needs it now?");
-
 window.onload = init;
 
 function init() {
   let button = document.getElementById('add_button');
   button.onclick = createSticky;
 
-  for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    if (key.substring(0, 6) === 'sticky') {
-      let value = localStorage.getItem(key);
-      addStickyToDOM(value);
-    }
+  let stickiesArray = getStickiesArray();
+
+  for (let i = 0; i < stickiesArray.length; i++) {
+    let key = stickiesArray[i];
+    let value = localStorage[key];
+    addStickyToDOM(value);
   }
 }
 
@@ -32,9 +28,26 @@ function addStickyToDOM(value) {
 }
 
 function createSticky() {
+  let stickiesArray = getStickiesArray();
+  let currentDate = new Date();
+  let key = "sticky_" + currentDate.getTime();
   let value = document.getElementById('note_text').value;
-  let key = "sticky_" + localStorage.length;
   localStorage.setItem(key, value);
+  stickiesArray.push(key);
+  localStorage.setItem('stickiesArray', JSON.stringify(stickiesArray));
 
   addStickyToDOM(value);
+}
+
+function getStickiesArray() {
+  let stickiesArray = localStorage.getItem('stickiesArray');
+
+  if (!stickiesArray) {
+    stickiesArray = [];
+    localStorage.setItem('stickiesArray', JSON.stringify(stickiesArray));
+  } else {
+    stickiesArray = JSON.parse(stickiesArray);
+  }
+
+  return stickiesArray;
 }
